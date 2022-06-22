da13e1f4da502bc4a8a432bfdcac4a093c371dbd
Muhammad Muizzsudin <muizzsuddin@privyid.tech>
2022-06-22 14:40:30 +0700

    Load from URL then HTTPClient load from the URL

--- Modules/DomainModule/Tests/DomainModuleTests.swift
+++ Modules/DomainModule/Tests/DomainModuleTests.swift
@@ -1,24 +1,26 @@
 import XCTest
 
 final class HTTPClient {
-    static let shared = HTTPClient()
-    
     var loadCount = 0
+    var urls = [URL]()
     
-    func load() {
+    func load(url: URL) {
         loadCount += 1
+        urls.append(url)
     }
 }
 
 final class RemoteLoadDocument {
     let httpClient: HTTPClient
+    let url: URL
     
-    init(httpClient: HTTPClient) {
+    init(url: URL = URL(string: "https://a.com")!, httpClient: HTTPClient) {
+        self.url = url
         self.httpClient = httpClient
     }
     
     func load() {
-        httpClient.load()
+        httpClient.load(url: url)
     }
 }
 
@@ -32,12 +34,25 @@ final class DomainModuleTests: XCTestCase {
     }
     
     func test_loadTwice_httpClientLoadTwice() {
-        let httpClient = HTTPClient.shared
+        let httpClient = HTTPClient()
         let sut = RemoteLoadDocument(httpClient: httpClient)
         
         sut.load()
         sut.load()
         
         XCTAssertEqual(httpClient.loadCount, 2)
+    }
+    
+    func test_loadURL_httpClientLoadFromTheURL() {
+        let httpClient = HTTPClient()
+        let url = URL(string: "https://a-url.com")!
+        let sut = RemoteLoadDocument(
+            url: url,
+            httpClient: httpClient
+        )
+        
+        sut.load()
+        
+        XCTAssertEqual(httpClient.urls, [url])
     }
 }
